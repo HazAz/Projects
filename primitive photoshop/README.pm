6438907cf2ad8d1b521df762413e42b75862126d
giron777 <31572226+giron777@users.noreply.github.com>
2017-12-22 02:22:18 -0500

    Create README.pm

---
+++ primitive photoshop/README.pm
@@ -0,0 +1,2 @@
+This was an assignment for my first comp course. Here I had to create a primitive photoshop program.
+It would take as input an image (PNM or PMG) and edit it (turning it to greyscale, rotating, flipping) by changing the pixels/ pixel arrangement
